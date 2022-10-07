888507fabe839a483eaaad6b8f8d688e8a6896cc
jguegon966 <jguegon966@g.educaand.es>
2022-10-07 14:25:13 +0200

    cambio1

--- lib/screens/home_screen copy.dart
+++ lib/screens/home_screen copy.dart
@@ -1,43 +1,50 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+
+  int contador = 0;
 
   @override
   Widget build(BuildContext context) {
     const textStyle = TextStyle(fontSize: 40);
-
+    
     return Scaffold(
       backgroundColor: Colors.grey,
       appBar: AppBar(
-        title: const Text('App de pruebas'),
+        title: const Text('App contador'),
         elevation: 10,
       ),
       body: Center(
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const <Widget>[
-          Text(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          const Text(
             'Numero de Clicks',
             style: textStyle,
           ),
           Text(
-            '10',
+            '$contador',
             style: textStyle,
           )
         ],
       )),
       floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
       floatingActionButton: FloatingActionButton(
-        child: Column(children: const <Widget>[
-          Icon(Icons.add),
-          Icon(Icons.remove)
-        ],), 
+        child: Column(
+          children: const <Widget>[Icon(Icons.add), Icon(Icons.remove)],
+        ),
         onPressed: () {
-          print('Hola mundo');
+          contador++;
+          setState(() {});
         },
-        
       ),
     );
   }
